1f58077c03c1d660960e1c3fc9d744b8e2e660d3
ling.gao <ling.gao@gmail.com>
2017-11-12 10:31:12 -0500

    alert message

--- views/home.py
+++ views/home.py
@@ -6,23 +6,19 @@ home_blueprint = Blueprint('home_blueprint', __name__,template_folder='templates
 #home page
 @home_blueprint.route('/home')
 def home():
-    try:
         if users.select().where(users.user_accesskey == session['key']).count() == 1:
             username = users.select().where(users.user_accesskey == session['key']).get().user_name
-    except:
-        return '''Database Error!'''
+            return render_template('home.html',renderData=username)
+        else:
+            return '''Please log in!'''
 
-    return render_template('home.html',renderData=username)
+
 
 @home_blueprint.route('/logout',methods=['POST'])
 def logout():
-    try:
-        if session['key'] != None:
-            users.update(user_status='offline').where(users.user_id==session['key'])
-            session.pop('key', None)
-            return redirect(url_for('index_blueprint.index'))
-        else:
-            return '''Already log out'''
-    except:
-        return '''Database Error!'''
-    return redirect(url_for('index_blueprint.index'))
+    if session['key'] != None:
+        users.update(user_status='offline').where(users.user_id==session['key'])
+        session.pop('key', None)
+        return redirect(url_for('index_blueprint.index'))
+    else:
+        return '''Already log out!'''
